107bb224599926ddf3e3fbef5ecfc665709baced
Jeremy <jeremyyamaako@gmail.com>
2023-11-19 23:53:08 +0200

    Add SQL schema

---
+++ schema.sql
@@ -0,0 +1,30 @@
+CREATE TABLE group_members (
+    id SERIAL PRIMARY KEY,
+    group_id INTEGER REFERENCES groups(id),
+    user_id INTEGER REFERENCES users(id),
+);
+
+CREATE TABLE groups (
+    id SERIAL PRIMARY KEY,
+    name TEXT
+);
+
+CREATE TABLE project_files (
+    id SERIAL PRIMARY KEY,
+    project_id INTEGER REFERENCES projects(id),
+    file_path TEXT
+);
+
+CREATE TABLE projects (
+    id SERIAL PRIMARY KEY,
+    name TEXT,
+    group_id INTEGER REFERENCES groups(id)
+);
+
+CREATE TABLE users (
+    id SERIAL PRIMARY KEY,
+    username TEXT,
+    password TEXT
+);
+
+
