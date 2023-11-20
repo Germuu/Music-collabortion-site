271bf408aed2d3ff8ca974ca787ab639d2761f04
Jeremy <jeremyyamaako@gmail.com>
2023-11-20 02:25:16 +0200

    Debugging schema more

--- schema.sql
+++ schema.sql
@@ -34,5 +34,5 @@ CREATE TABLE project_files (
 CREATE TABLE group_members (
     id SERIAL PRIMARY KEY,
     group_id INTEGER REFERENCES groups(id),
-    user_id INTEGER REFERENCES users(id),
+    user_id INTEGER REFERENCES users(id)
 );
